b926f2838912e8f82f2be02ff60e7508dcb2455d
William Chang <none>
2025-04-04 14:17:58 -0700

    Fix typo in comment

--- llama3/model.py
+++ llama3/model.py
@@ -98,7 +98,7 @@ def feed_forward(params, x):
   # Implementation is agnostic of batch and context length, so pretending x is
   # shape (dim,) the implentation does the following:
   # x2 = dot(x, w1) - Project x to shape (4 * dim,).
-  # gate = swilu(dot(x, w2)) - Create gate also of shape (4 * dim,).
+  # gate = silu(dot(x, w2)) - Create gate also of shape (4 * dim,).
   # output = dot(x2 * gate, w3) - Gate x2 and then project back down to shape (dim,)
   return jnp.dot(
       jnp.dot(x, params['w1']) *
